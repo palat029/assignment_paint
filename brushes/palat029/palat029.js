85ba9ec1420370a50d604681260791ac517c9a92
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-11-06 15:25:36 -0500

    Lollipop and Face

--- brushes/palat029/palat029.js
+++ brushes/palat029/palat029.js
@@ -11,6 +11,7 @@
 
 var palat029 = new Brush("palat029", "./brushes/palat029/palat029.svg");
 brushes.push(palat029);
+var mouseWasPressed;
 
 
 
@@ -22,6 +23,19 @@ palat029.draw = function() {
 	stroke(forecolor);
 
 	if (mouseIsPressed) {
-		rect(pmouseX, pmouseY, mouseX + random(0, 10), mouseY + random(-3, 3));
+		line(pmouseX, pmouseY, mouseX - 10, mouseY + 30, mouseX + 85, mouseY + 30);
+		ellipse(pmouseX, pmouseY, 20, 20);
+
 	}
+};
+palat029.mouseReleased = function() {
+	fill(255, 255, 0);
+	stroke(forecolor);
+
+	ellipse(pmouseX + 35, pmouseY + 35, 70, 70);
+	fill(0, 0, 0);
+	ellipse(pmouseX + 37, pmouseY + 20, 10, 10);
+	ellipse(pmouseX + 20, pmouseY + 20, 10, 10);
+	fill(255, 255, 255);
+	ellipse(pmouseX + 25, pmouseY + 50, 20, 30);
 };
